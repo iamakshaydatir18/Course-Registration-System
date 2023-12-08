54849a4be64f616b56dc1d54e45a97f71b27d9a6
Sangeet Saha <saha.san@northeastern.edu>
2023-12-08 10:23:03 -0500

    Added modal to admin page

--- src/client/src/pages/Adminpage.jsx
+++ src/client/src/pages/Adminpage.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useState } from 'react';
+import Modal from 'react-modal';
 
 const Adminpage = () => {
 
@@ -10,6 +11,33 @@ const Adminpage = () => {
         'Intro to DSA',
         'Object Oriented Design',
       ]);
+
+    const [newCourse, setNewCourse] = useState();
+    
+    const [modalIsOpen, setIsOpen] = React.useState(false);
+
+    const openModal = () => {
+        setIsOpen(true);
+    }
+    const closeModal = () => {
+        setIsOpen(false);
+    }
+
+    const customStyles = {
+        content: {
+          top: '50%',
+          left: '50%',
+          right: 'auto',
+          bottom: 'auto',
+          marginRight: '-50%',
+          transform: 'translate(-50%, -50%)',
+        },
+    };
+
+    let subtitle;
+    function afterOpenModal() {
+        subtitle.style.color = '#f00';
+    }
 
   return (
     <div>
@@ -22,7 +50,25 @@ const Adminpage = () => {
                 </div>
             ))}
         </div>
-        <h2><button>Add a new course</button></h2>
+        <h2><button onClick={()=>openModal()}>Add a new course</button></h2>
+        <Modal
+            isOpen={modalIsOpen}
+            onAfterOpen={afterOpenModal}
+            onRequestClose={()=>closeModal()}
+            style={customStyles}
+            contentLabel="Example Modal"
+        >
+            <h2 ref={(_subtitle) => (subtitle = _subtitle)}>Example Modal</h2>
+            <button onClick={closeModal}>close</button>
+            <div>I am a modal</div>
+            
+            <input />
+            <button>tab navigation</button>
+            <button>stays</button>
+            <button>inside</button>
+            <button>the modal</button>
+            
+      </Modal>
     </div>
   )
 }
